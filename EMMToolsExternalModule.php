de426c241e95dc6f3f61497fa62b2199427fbdbc
Günther Rezniczek <g.rezniczek@gmail.com>
2020-04-13 18:27:35 +0200

    Fix isModuleEnabled.

--- EMMToolsExternalModule.php
+++ EMMToolsExternalModule.php
@@ -114,54 +114,54 @@ class EMMToolsExternalModule extends AbstractExternalModule {
 
 
         // MySQL Simple Admin Shortcut
-        if ($this->_isModuleEnabled("mysql_simple_admin")) {
-            if ($this->getSystemSetting("mysql-simple-admin-links")) {
-                if (PageInfo::IsProjectExternalModulesManager()) {
-                    $link = (PageInfo::IsDevelopmentFramework($this) ? APP_PATH_WEBROOT_PARENT . "external_modules" : APP_PATH_WEBROOT . "ExternalModules") . "/?prefix=mysql_simple_admin&page=index&query-pid={$project_id}&module-prefix=";
-                    ?>
-                    <script>
-                        $(function(){
-                            $('#external-modules-enabled tr[data-module]').each(function() {
-                                var tr = $(this)
-                                var moduleName = tr.attr('data-module')
-                                var link = $('<a target="_blank" href="<?=$link?>' + moduleName + '" style="margin-right:1em;"><i class="fas fa-database" style="margin-right:2px;"></i> <?=$fw->tt("mysqllink_label")?></a>')
-                                var td = tr.find('td').first();
-                                if (td.find('div.external-modules-byline').length) {
-                                    var div = td.find('div.external-modules-byline').first()
-                                    div.append(link)
-                                }
-                                else {
-                                    var div = $('<div class="external-modules-byline"></div>')
-                                    div.append(link)
-                                    link.css('display', 'block')
-                                    link.css('margin-top', '7px')
-                                    td.append(div)
-                                }
-                            })
+        if ((PageInfo::IsProjectExternalModulesManager() || PageInfo::IsMySQLSimpleAdmin()) &&
+            $this->getSystemSetting("mysql-simple-admin-links") && 
+            $this->_isModuleEnabled("mysql_simple_admin")) {
+            if (PageInfo::IsProjectExternalModulesManager()) {
+                $link = (PageInfo::IsDevelopmentFramework($this) ? APP_PATH_WEBROOT_PARENT . "external_modules" : APP_PATH_WEBROOT . "ExternalModules") . "/?prefix=mysql_simple_admin&page=index&query-pid={$project_id}&module-prefix=";
+                ?>
+                <script>
+                    $(function(){
+                        $('#external-modules-enabled tr[data-module]').each(function() {
+                            var tr = $(this)
+                            var moduleName = tr.attr('data-module')
+                            var link = $('<a target="_blank" href="<?=$link?>' + moduleName + '" style="margin-right:1em;"><i class="fas fa-database" style="margin-right:2px;"></i> <?=$fw->tt("mysqllink_label")?></a>')
+                            var td = tr.find('td').first();
+                            if (td.find('div.external-modules-byline').length) {
+                                var div = td.find('div.external-modules-byline').first()
+                                div.append(link)
+                            }
+                            else {
+                                var div = $('<div class="external-modules-byline"></div>')
+                                div.append(link)
+                                link.css('display', 'block')
+                                link.css('margin-top', '7px')
+                                td.append(div)
+                            }
                         })
-                    </script>
-                    <?php
-                }
-                if (PageInfo::IsMySQLSimpleAdmin()) {
-                    $prefix = $_GET["module-prefix"];
-                    $pid = $_GET["query-pid"];
-                    $result = $fw->query("
-                        select external_module_id 
-                        from redcap_external_modules 
-                        where directory_prefix = ?",
-                        [ $prefix ]);
-                    $module_id = ($result->fetch_assoc())["external_module_id"];
-                    $query = "select * from redcap_external_module_settings where external_module_id = {$module_id} and project_id = {$pid}";
-                    if ($module_id !== null) {
-                    ?>
-                    <script>
-                        $(function() {
-                            $('#query').val(<?=json_encode($query)?>)
-                            $('#form button').click()
-                        })
-                    </script>
-                    <?php
-                    }
+                    })
+                </script>
+                <?php
+            }
+            if (PageInfo::IsMySQLSimpleAdmin()) {
+                $prefix = $_GET["module-prefix"];
+                $pid = $_GET["query-pid"];
+                $result = $fw->query("
+                    select external_module_id 
+                    from redcap_external_modules 
+                    where directory_prefix = ?",
+                    [ $prefix ]);
+                $module_id = ($result->fetch_assoc())["external_module_id"];
+                $query = "select * from redcap_external_module_settings where external_module_id = {$module_id} and project_id = {$pid}";
+                if ($module_id !== null) {
+                ?>
+                <script>
+                    $(function() {
+                        $('#query').val(<?=json_encode($query)?>)
+                        $('#form button').click()
+                    })
+                </script>
+                <?php
                 }
             }
         }
@@ -177,7 +177,7 @@ class EMMToolsExternalModule extends AbstractExternalModule {
      * @throws InvalidArgumentException
      **/
     public function _isModuleEnabled($prefix, $pid = null) {
-        if (method_exists($this, "isModuleEnabled")) {
+        if (method_exists($this->framework, "isModuleEnabled")) {
             return $this->isModuleEnabled($prefix, $pid);
         }
         else {
